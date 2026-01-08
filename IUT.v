ea112756923f478e27478a384ea5bded11d1d6c7
CharlesCNorton <machineelv@gmail.com>
2026-01-08 12:47:10 -0500

    Add core IUT structures: Frobenioids, prime strips, pilot objects, Scholze-Stix diagram, Corollary 3.12 scaffold

--- IUT.v
+++ IUT.v
@@ -48,4 +48,188 @@ Section Foundations.
 (*  - Basic category-theoretic infrastructure                                 *)
 (******************************************************************************)
 
+Variable R : realType.
+
+Definition Place := nat.
+Definition BadPlace := nat.
+
+Record NumberFieldData := mkNFData {
+  nf_degree : nat;
+  nf_places : seq Place;
+  nf_bad_places : seq BadPlace
+}.
+
 End Foundations.
+
+(******************************************************************************)
+(*                                                                            *)
+(*                    PART II: GLOBAL REALIFIED FROBENIOIDS                   *)
+(*                                                                            *)
+(******************************************************************************)
+
+Section GlobalRealifiedFrobenioids.
+
+Variable R : realType.
+
+Record OrderedRVectorSpace := mkORVS {
+  orvs_carrier :> Type;
+  orvs_zero : orvs_carrier;
+  orvs_add : orvs_carrier -> orvs_carrier -> orvs_carrier;
+  orvs_scale : R -> orvs_carrier -> orvs_carrier;
+  orvs_le : orvs_carrier -> orvs_carrier -> bool
+}.
+
+Record GlobalRealifiedFrobenioid := mkGRF {
+  grf_vspaces : Place -> OrderedRVectorSpace;
+  grf_degree_zero_subspace : forall v, (grf_vspaces v)
+}.
+
+End GlobalRealifiedFrobenioids.
+
+(******************************************************************************)
+(*                                                                            *)
+(*                      PART III: PRIME STRIPS                                *)
+(*                                                                            *)
+(******************************************************************************)
+
+Section PrimeStrips.
+
+Variable R : realType.
+Variable F : fieldType.
+
+Record LocalData (v : Place) := mkLocalData {
+  ld_galois_group : finType;
+  ld_units : {set ld_galois_group};
+  ld_monoid_gen : nat
+}.
+
+Record FPrimeStrip := mkFPS {
+  fps_places : seq Place;
+  fps_local_data : forall v, LocalData v;
+  fps_global_frobenioid : GlobalRealifiedFrobenioid R
+}.
+
+End PrimeStrips.
+
+(******************************************************************************)
+(*                                                                            *)
+(*                       PART IV: PILOT OBJECTS                               *)
+(*                                                                            *)
+(******************************************************************************)
+
+Section PilotObjects.
+
+Variable R : realType.
+Variable ell : nat.
+
+Definition ThetaIndex := 'I_ell.
+
+Record QPilotObject := mkQPilot {
+  qpilot_values : Place -> R;
+  qpilot_deg : R
+}.
+
+Record ThetaPilotObject := mkThetaPilot {
+  thetapilot_values : ThetaIndex -> Place -> R;
+  thetapilot_deg : ThetaIndex -> R
+}.
+
+Definition j_squared (j : nat) : nat := j * j.
+
+End PilotObjects.
+
+(******************************************************************************)
+(*                                                                            *)
+(*                 PART V: THE SCHOLZE-STIX DIAGRAM                           *)
+(*                                                                            *)
+(******************************************************************************)
+
+Section ScholzeStixDiagram.
+
+Variable R : realType.
+Variable ell : nat.
+
+Record RIdentification := mkRId {
+  rid_source : R;
+  rid_target : R;
+  rid_scale : R
+}.
+
+Record SSVertex := mkSSV {
+  ssv_name : nat;
+  ssv_value : R
+}.
+
+Record SSDiagram := mkSSD {
+  ssd_R_theta_theta : SSVertex;
+  ssd_R_theta_q : SSVertex;
+  ssd_R_c_theta : nat -> SSVertex;
+  ssd_R_c_q : SSVertex;
+  ssd_R_theta : SSVertex;
+  ssd_R_q : SSVertex;
+  ssd_theta_link : RIdentification;
+  ssd_concrete_to_abstract_theta : nat -> RIdentification;
+  ssd_concrete_to_abstract_q : RIdentification;
+  ssd_abstract_to_arith_theta : RIdentification;
+  ssd_abstract_to_arith_q : RIdentification
+}.
+
+Definition j_scaling (j : nat) : nat := j * j.
+
+Definition diagram_has_monodromy (D : SSDiagram) : Prop :=
+  exists j : nat, (j > 0)%N /\
+    (ssd_concrete_to_abstract_theta D j).(rid_scale) <> 1.
+
+Definition diagram_commutes (D : SSDiagram) : Prop :=
+  forall j : nat, (j > 0)%N ->
+    (ssd_concrete_to_abstract_theta D j).(rid_scale) = 1.
+
+End ScholzeStixDiagram.
+
+(******************************************************************************)
+(*                                                                            *)
+(*              PART VI: COROLLARY 3.12 - THE DISPUTED CLAIM                  *)
+(*                                                                            *)
+(******************************************************************************)
+
+Section Corollary312.
+
+Variable R : realType.
+Variable ell : nat.
+
+Definition log_volume := R.
+
+Record Cor312Statement := mkCor312 {
+  cor312_log_theta : log_volume;
+  cor312_log_q : log_volume;
+  cor312_inequality : cor312_log_theta <= cor312_log_q
+}.
+
+Definition mochizuki_interpretation (j : nat) : nat := j * j.
+
+Definition scholze_stix_interpretation (j : nat) : nat := j.
+
+Definition sum_with_j_squared (ell : nat) : nat :=
+  \sum_(1 <= j < ell.+1) (j * j).
+
+Definition sum_without_j_squared (ell : nat) : nat :=
+  \sum_(1 <= j < ell.+1) j.
+
+End Corollary312.
+
+(******************************************************************************)
+(*                                                                            *)
+(*                    PART VII: THE CORE DISPUTE                              *)
+(*                                                                            *)
+(******************************************************************************)
+
+Section CoreDispute.
+
+Variable R : realType.
+
+Definition vacuous_inequality (delta : R) : Prop := 0 <= delta.
+
+Definition useful_inequality (deg_q delta : R) (C : R) : Prop :=
+  deg_q <= C * delta.
+
+End CoreDispute.
